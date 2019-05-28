b353b6fadda3bfb49bda435bce903243b782f1e8
css <1073962756@qq.com>
2019-05-28 11:18:07 +0800

    This is the second verdion

--- src/index.js
+++ src/index.js
@@ -4,20 +4,7 @@ import ReactDOM from "react-dom"
 
 //创建虚拟DOM 不使用这种方式创建虚拟DOM 太麻烦了 使用JSX语法 需要安装babel
 // let element = React.createElement("h1", null, "这只是一个实验");
-class Practice extends React.Component {
-    constructor() {
-        super();
 
-    }
-
-    render() {
-        return <div onClick={ () => this.show() }>123456789</div>
-    }
-
-    show = () => {
-        console.log(123456789);
-    }
-}
 
 //挂载渲染虚拟DOM
 ReactDOM.render(<Practice></Practice>, document.getElementById("app"));
