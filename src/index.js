10ddd389f9d666d21b6fcef42a419102a8bf618f
css <1073962756@qq.com>
2019-05-29 09:37:27 +0800

    This is th entry file

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
+//导入自己的父组件
+import ItemWrap from "@/components/ItemWrap.jsx"
+
 //创建虚拟DOM 不使用这种方式创建虚拟DOM 太麻烦了 使用JSX语法 需要安装babel
 // let element = React.createElement("h1", null, "这只是一个实验");
 
 
-//挂载渲染虚拟DOM
-ReactDOM.render(<Practice></Practice>, document.getElementById("app"));
+//挂载渲染虚拟DOM 直接将父组件放到这里
+ReactDOM.render(<div><ItemWrap></ItemWrap></div>, document.getElementById("app"));
 
