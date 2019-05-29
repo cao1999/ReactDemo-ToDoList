7585948bc60ff8fa1a801cfb5f37129a4c084741
css <1073962756@qq.com>
2019-05-29 09:36:42 +0800

    This is the webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,22 @@ module.exports = {
     //这里是module 不是modules
     module: {
         rules: [
-            {test: /\.js|jsx/, use: "babel-loader", exclude: /node_modules/}
+            //配置js和jsx文件的loader
+            {test: /\.js|jsx$/, use: "babel-loader", exclude: /node_modules/},
+            //配置css文件的loader 不需要模块化处理
+            {test: /\.css$/, use: ["style-loader", "css-loader"]},
+            //配置scss文件的loader 需要模块化
+            {test: /\.scss$/, use: ["style-loader", "css-loader?modules", "sass-loader"]},
+            //配置图片的loader
+            {test: /\.jpg|png|gif$/, use: "url-loader"}
         ]
+    },
+
+    resolve: {
+        extensions: [".js", ".jsx", ".json"],
+        //配置别名
+        alias: {
+            "@": path.join(__dirname, "./src")
+        }
     }
 }
