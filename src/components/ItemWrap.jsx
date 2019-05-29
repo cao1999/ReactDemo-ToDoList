eef2c6d25075a09604dc67973a5be7b1fb68d7cb
css <1073962756@qq.com>
2019-05-29 09:38:17 +0800

    This is a component

--- src/components/ItemWrap.jsx
+++ src/components/ItemWrap.jsx
@@ -1,17 +1,24 @@
 //这里是父组件 最外围的元素
-
 //每个组件都要导入react包
 import React from "react"
 
+//导入自己的子组件
+import ChildInput from "@/components/ChildInput.jsx"
+import ChildImage from "@/components/ChildImage.jsx"
+
+//导出这个组件
 export default class ItemWrap extends React.Component {
     constructor() {
         super();
         this.state = {
-
+            
         }
     }
 
     render() {
-        return null;
+        return <div>
+            <ChildImage></ChildImage>
+            <ChildInput></ChildInput>
+        </div>;
     }
 }
